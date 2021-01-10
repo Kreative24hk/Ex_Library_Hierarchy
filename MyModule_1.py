0bbec8d5c5514257994e23062d64d6a194e0271c
Kreative24hk <57408825+Kreative24hk@users.noreply.github.com>
2021-01-10 22:44:18 +0900

    Add files via upload

---
+++ MyModule_1.py
@@ -0,0 +1,11 @@
+# This is the example of MODULE in Python.
+# created by Kaden. 01.10.2020
+
+def MyMethod_1():
+    print ("This is MyMethod_1 in MyModule_1.py")
+
+class MyClass_1():
+    MyVariable_1 = "This is MyVariable_1 in MyClass_1 of MyModule_1.py"
+    MyVariable_2 = "This is MyVariable_2 in MyClass_1 of MyModule_1.py"
+    def MyMethod_2(self):
+        print ("This is MyMethod_2 in MyClass_1 of MyModule_1.py")
---
+++ __init__.py
@@ -0,0 +1,4 @@
+# This is the example of MODULE in Python.
+# created by Kaden. 01.10.2020
+
+print ("__init__.py inside the PACKAGE:Ex_Library_Hierarchy is executed ...")
